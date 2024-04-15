017b78bfe3ab499e8c39f75a494643497d9d443f
Benjamin Marwell <bmarwell@apache.org>
2024-04-15 15:44:20 +0200

    fix license header

--- application/application-api/src/main/java/io/github/bmarwell/keyserver/application/api/RepositoryService.java
+++ application/application-api/src/main/java/io/github/bmarwell/keyserver/application/api/RepositoryService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023-2024 The SIPper project team.
+ * Copyright (C) 2023-2024 The java-keyserver project team.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
